0598a6da5e57a9a8c29b82540e099e102b68a2d5
Yuki Ota <yukiotataitien@gmail.com>
2021-10-04 22:18:32 +0800

    CICD TEST

--- src/App.vue
+++ src/App.vue
@@ -14,7 +14,7 @@
           <v-toolbar-title
             style="font-family: 'Pacifico', cursive !important;">
             I Running
-            TEST3
+            TEST4
           </v-toolbar-title>
         </router-link>
         <v-spacer></v-spacer>
